cfd29997544b69e323c6d46a2c5ed9a9e88276f5
Isamu Arimoto <isamu@to-kyo.to>
2021-01-02 08:15:53 +0900

    add test

---
+++ tests/api_test.js
@@ -0,0 +1,26 @@
+import SmaregiApi from "../src/";
+
+test('api test', async () => {
+  const config = {
+    contractId: process.env.SmaregiContractId,
+    clientId: process.env.SmaregiClientId,
+    clientSecret: process.env.SmaregiClientSecret,
+    hostName: "api.smaregi.dev",
+    scopes: [
+      "pos.stock:read", "pos.stock:write",
+      "pos.stores:read", "pos.stores:write",
+      "pos.customers:read", "pos.customers:write", 
+      "pos.products:read", "pos.products:write"
+    ]
+  };
+  
+  const api = new SmaregiApi(config);
+  await api.auth();
+
+  const storesApi = api.stores();
+  const storeList = await storesApi.list();
+  expect(storeList).toBeInstanceOf(Array)
+  
+  // console.log(storeList);
+  
+});
